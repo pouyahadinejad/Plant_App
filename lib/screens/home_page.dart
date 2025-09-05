28dc1ac71f8198001d193d3a508e46a0553d2660
pouyahadinejad <pouya.hadi1382@gmail.com>
2025-09-05 11:18:21 +0330

    end spalsh

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,201 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/const/constans.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final PageController _pageController = PageController(initialPage: 0);
+  int currentIndex = 0;
+
+  Widget _indicator(bool isActive) {
+    return AnimatedContainer(
+      duration: const Duration(microseconds: 300),
+      height: 10.0,
+      width: isActive ? 20.0 : 8.0,
+      margin: const EdgeInsets.only(right: 5.0),
+      decoration: BoxDecoration(
+        color: Constans.primaryColor,
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+    );
+  }
+
+  List<Widget> _buildIndicator() {
+    List<Widget> indicator = [];
+    for (int i = 0; i < 3; i++) {
+      if (currentIndex == i) {
+        indicator.add(_indicator(true));
+      } else {
+        indicator.add(_indicator(false));
+      }
+    }
+    return indicator;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0, right: 20.0),
+            child: InkWell(
+              onTap: () {},
+              child: const Text(
+                "رد کردن",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16.0,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+      body: Stack(
+        children: [
+          PageView(
+            onPageChanged: (int page) {
+              setState(() {
+                currentIndex = page;
+              });
+            },
+            controller: _pageController,
+            children: [
+              CreatePage(
+                title: Constans.titleOne,
+                image: "assets/images/plant-one.png",
+                description: Constans.descriptionOne,
+              ),
+              CreatePage(
+                title: Constans.titleTwo,
+                image: "assets/images/plant-two.png",
+                description: Constans.descriptionTwo,
+              ),
+              CreatePage(
+                title: Constans.titleThree,
+                image: "assets/images/plant-three.png",
+                description: Constans.descriptionThree,
+              ),
+            ],
+          ),
+          Positioned(
+            bottom: 80.0,
+            left: 30.0,
+            child: Row(
+              children: _buildIndicator(),
+            ),
+          ),
+          Positioned(
+            bottom: 60.0,
+            right: 30.0,
+            child: Container(
+              padding: const EdgeInsets.all(4.0),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Constans.primaryColor,
+              ),
+              child: IconButton(
+                onPressed: () {
+                  setState(() {
+                    if (currentIndex < 2) {
+                      currentIndex++;
+                      if (currentIndex < 3) {
+                        _pageController.nextPage(
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.easeIn,
+                        );
+                      } else {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const LoginPage(),
+                          ),
+                        );
+                      }
+                    }
+                  });
+                },
+                icon: const Icon(
+                  Icons.arrow_forward_ios,
+                  size: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CreatePage extends StatelessWidget {
+  final String title;
+  final String image;
+  final String description;
+  const CreatePage({
+    super.key,
+    required this.title,
+    required this.image,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        right: 55.0,
+        left: 55.0,
+        bottom: 80.0,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 350.0,
+            child: Image.asset(image),
+          ),
+          const SizedBox(height: 20.0),
+          Text(
+            title,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              color: Constans.primaryColor,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 20.0),
+          Text(
+            description,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 20.0,
+              color: Colors.grey,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          const SizedBox(height: 20.0),
+        ],
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
