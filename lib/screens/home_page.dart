e5fc794684a670f74a13b0e0caf59ed682ac476e
pouyahadinejad <pouya.hadi1382@gmail.com>
2025-09-13 15:30:16 +0330

    16

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -9,6 +9,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int selectedIndex = 0;
+  final List<String> _plantTypes = [
+    '|پیشنهادی|',
+    '|آپارتمانی|',
+    '|محل کار|',
+    '|گل باغچه ای|',
+    '|گل سمی|',
+  ];
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -20,7 +28,7 @@ class _HomePageState extends State<HomePage> {
               padding: const EdgeInsets.only(top: 20.0),
               child: Center(
                 child: Container(
-                  padding: const EdgeInsets.symmetric(vertical: 12.0),
+                  padding: const EdgeInsets.symmetric(),
                   width: size.width * 0.9,
                   decoration: BoxDecoration(
                     color: Constans.primaryColor.withOpacity(0.1),
@@ -30,7 +38,10 @@ class _HomePageState extends State<HomePage> {
                     crossAxisAlignment: CrossAxisAlignment.center,
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const Icon(Icons.search),
+                      Icon(
+                        Icons.mic,
+                        color: Constans.primaryColor.withOpacity(0.6),
+                      ),
                       const Expanded(
                         child: Directionality(
                           textDirection: TextDirection.rtl,
@@ -46,13 +57,44 @@ class _HomePageState extends State<HomePage> {
                           ),
                         ),
                       ),
-                      Icon(
-                        Icons.mic,
-                        color: Constans.primaryColor.withOpacity(0.6),
-                      )
+                      const Icon(Icons.search),
                     ],
                   ),
                 ),
+              ),
+            ),
+            Container(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 18.0, vertical: 10.0),
+              height: 70.0,
+              width: size.width,
+              child: ListView.builder(
+                reverse: true,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedIndex = index;
+                        });
+                      },
+                      child: Text(
+                        _plantTypes[index],
+                        style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: selectedIndex == index
+                                ? FontWeight.bold
+                                : FontWeight.w300,
+                            color: selectedIndex == index
+                                ? Constans.primaryColor
+                                : Constans.blackColor),
+                      ),
+                    ),
+                  );
+                },
+                scrollDirection: Axis.horizontal,
+                itemCount: _plantTypes.length,
               ),
             ),
           ],
