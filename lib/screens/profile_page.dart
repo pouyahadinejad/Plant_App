558717912f4bcf6842f48329f00d0c7c3ffb5c51
pouyahadinejad <pouya.hadi1382@gmail.com>
2025-09-17 11:38:05 +0330

    41

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/const/constans.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -10,9 +11,33 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("پروفایل کاربر"),
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(20.0),
+        height: size.height,
+        width: size.height,
+        child: Column(
+          children: [
+            Container(
+              width: 150.0,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Constans.primaryColor.withOpacity(0.5),
+                  width: 5.0,
+                ),
+              ),
+              child: const CircleAvatar(
+                radius: 60.0,
+                backgroundColor: Colors.transparent,
+                backgroundImage: ExactAssetImage(
+                  "assets/images/profile.jpg",
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
